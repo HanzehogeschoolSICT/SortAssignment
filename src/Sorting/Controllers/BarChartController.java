27d10e34765ef317d7a75aed42bfc7d9ec2bc837
Koen Hendriks <info@koenhendriks.com>
2017-03-02 12:56:01 +0100

    Dynamicly call barchart in the tab view

--- src/Sorting/Controllers/BarChartController.java
+++ src/Sorting/Controllers/BarChartController.java
@@ -12,13 +12,13 @@ import java.util.Random;
  * Created by koen on 3/2/17.
  */
 public class BarChartController {
-    private BarChart<String, Number> getRandomBarChart(int N) {
+    public static BarChart<String, Number> getRandomBarChart(int N) {
         CategoryAxis xAxis = new CategoryAxis();
         NumberAxis yAxis = new NumberAxis();
 
         BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
 
-        ArrayList<Integer> randomList = this.getRandomArrayList(N);
+        ArrayList<Integer> randomList = BarChartController.getRandomArrayList(N);
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("random numbers");
 
@@ -34,7 +34,7 @@ public class BarChartController {
         return bc;
     }
 
-    private ArrayList<Integer> getRandomArrayList(int N){
+    private static ArrayList<Integer> getRandomArrayList(int N){
         ArrayList<Integer> numbers = new ArrayList<>(N);
 
         for (int i = 1; i < N+1; i++) {
--- src/Sorting/Controllers/tabControllers/BubbleSortController.java
+++ src/Sorting/Controllers/tabControllers/BubbleSortController.java
@@ -1,10 +1,23 @@
 package Sorting.Controllers.tabControllers;
 
+import Sorting.Controllers.BarChartController;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * Created by koen on 3/2/17.
  */
 public class BubbleSortController{
+
+    @FXML
+    private AnchorPane bubbleSortAnchor;
+
+    private BarChart bc;
+
     public void initialize(){
-        System.out.println("bubble sort tab init");
+
+        this.bc = BarChartController.getRandomBarChart(20);
+        bubbleSortAnchor.getChildren().add(this.bc);
     }
 }
