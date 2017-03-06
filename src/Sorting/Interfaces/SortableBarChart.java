353c073ea27f9431b36881332ecf9e67eca3f2aa
Koen Hendriks <info@koenhendriks.com>
2017-03-06 22:03:22 +0100

    Created interface for sortable barcharts

---
+++ src/Sorting/Interfaces/SortableBarChart.java
@@ -0,0 +1,16 @@
+package Sorting.Interfaces;
+
+import javafx.scene.chart.BarChart;
+
+/**
+ * Class SortableBarChart
+ *
+ * @author Koen Hendriks
+ * @version 0.1 (06-03-2017)
+ */
+public interface SortableBarChart {
+
+    void step();
+
+    void drawBarChart();
+}
