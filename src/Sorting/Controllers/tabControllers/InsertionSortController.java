3598cac3dbc954c64de4af602c16802336715051
Ruben Buisman <rbuisman95@gmail.com>
2017-03-08 15:39:05 +0100

    Created initialize and interface functions to display barchart

--- src/Sorting/Controllers/tabControllers/InsertionSortController.java
+++ src/Sorting/Controllers/tabControllers/InsertionSortController.java
@@ -1,8 +1,11 @@
 package Sorting.Controllers.tabControllers;
 
+import Sorting.Controllers.BarChartController;
 import Sorting.Interfaces.SortableBarChart;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
 import javafx.scene.layout.AnchorPane;
+import java.util.List;
 
 /**
  * Class InsertionSortController
@@ -14,5 +17,27 @@ public class InsertionSortController implements SortableBarChart {
 
     @FXML
     private AnchorPane insertionSortAnchor;
+    private BarChart<String, Number> bc;
 
+
+    // Initialize the barchart
+    public void initialize(){
+        this.bc = BarChartController.getRandomBarChart(20);
+        drawBarChart();
+    }
+
+    @Override
+    public void drawBarChart() {
+        insertionSortAnchor.getChildren().add(this.bc);
+    }
+
+    @Override
+    public void step() {
+        // Does nothing yet.
+    }
+
+    @Override
+    public void redrawBarChart(List<Integer> data) {
+        // Does nothing yet.
+    }
 }
