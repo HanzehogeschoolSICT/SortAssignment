79c5fcc96512c8f0a1790ce9a2067a6e275efed3
Koen Hendriks <info@koenhendriks.com>
2017-03-09 15:44:33 +0100

    Added documentation and docblocks

--- src/Sorting/Controllers/tabControllers/BubbleSortController.java
+++ src/Sorting/Controllers/tabControllers/BubbleSortController.java
@@ -25,27 +25,47 @@ public class BubbleSortController extends AbstractSortController{
     private int finished = 0;
     private int speed = 100;
 
+    /**
+     * Initialize method that gets called by the FXML when the controller is injected.
+     */
+    @Override
     public void initialize(){
+        // Tell the AbstractSortController which pane and textfield we have.
         super.setAnchorPane(bubbleSortAnchor);
         super.setSpeedTextField(speedTextField);
+
+        // Initialize the AbstractSortController to create and draw the BarChart.
         super.initialize();
     }
 
     @Override
     public List<Integer> step() {
-        List<Integer> data = BarChartController.getSeriesData(super.bc);
+        // Get the current Y axis data from the BarChart as a List with integers
+        List<Integer> data = super.getSerieData();
 
+        // Stop running if we are finished
         if(finished >= data.size()){
             this.running = false;
         }else {
+            /*
+              Set the first two values for the Bubble Sort algorithm.
+              These values come from the BarChart with the offset that
+              we keep track on.
+             */
             int first = data.get(stepOffset);
             int second = data.get(stepOffset + 1);
 
+            // If the first value is bigger then second we swap them.
             if (first > second) {
                 data.set(stepOffset, second);
                 data.set(stepOffset + 1, first);
             }
 
+            /*
+             Subtract the finished values from the array size,
+             this way it won't repeat the step of the values
+             that are already sorted at the end of the array.
+              */
             if (stepOffset >= (data.size() - finished) - 2) {
                 stepOffset = 0;
                 finished++;
