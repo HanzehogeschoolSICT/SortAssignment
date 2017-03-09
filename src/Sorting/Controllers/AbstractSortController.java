d53933053d8e861715f23ccb206397f3252f21b5
Koen Hendriks <info@koenhendriks.com>
2017-03-09 15:39:28 +0100

    Optimized small code and added docblocks

--- src/Sorting/Controllers/AbstractSortController.java
+++ src/Sorting/Controllers/AbstractSortController.java
@@ -2,14 +2,12 @@ package Sorting.Controllers;
 
 import Sorting.Interfaces.SortableBarChart;
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * Class AbstractSortController
@@ -23,11 +21,15 @@ public abstract class AbstractSortController implements SortableBarChart{
     private TextField speedTextField;
     private AnchorPane anchorPane;
 
-    protected BarChart bc;
-    protected int speed;
+    private BarChart bc;
+    private int speed;
 
     protected boolean running = false;
 
+    /**
+     * Initialize a BarChart and draw the BarChart to the
+     * correct pane given by a child class.
+     */
     protected void initialize(){
         bc = BarChartController.getRandomBarChart(20);
         drawBarChart();
@@ -44,7 +46,8 @@ public abstract class AbstractSortController implements SortableBarChart{
     }
 
     /**
-     * Reset the tab view with a new barchart
+     * Reset the tab view with a new BarChart and stop
+     * the current sorting thread if its running.
      */
     public void resetButtonPressed() {
         anchorPane.getChildren().removeAll();
@@ -78,14 +81,17 @@ public abstract class AbstractSortController implements SortableBarChart{
         }).start();
     }
 
+    /**
+     * Draw the created BarChart to the given pane.
+     */
     public void drawBarChart() {
         anchorPane.getChildren().add(bc);
     }
 
     /**
-     * Redraw a barchart with new data
+     * Redraw a BarChart with new data
      *
-     * @param data List with the new values for the barchart
+     * @param data List with the new values for the BarChart
      */
     public void redrawBarChart(List<Integer> data) {
         bc.getData().remove(0);
@@ -98,11 +104,21 @@ public abstract class AbstractSortController implements SortableBarChart{
         bc.getData().add(serie);
     }
 
+    /**
+     * Send the BarChart to the static BarChart controller which
+     * will return the series Y axis data as an list with integers.
+     *
+     * @return List with integers of the Y axis
+     */
+    protected List<Integer> getSerieData(){
+        return (List<Integer>) BarChartController.getSeriesData(bc);
+    }
+
     protected void setAnchorPane(AnchorPane anchorPane) {
         this.anchorPane = anchorPane;
     }
 
-    public void setSpeedTextField(TextField speedTextField) {
+    protected void setSpeedTextField(TextField speedTextField) {
         this.speedTextField = speedTextField;
     }
 }
